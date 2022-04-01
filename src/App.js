36de568eede017418b9c52182ff5ef90f3444bf6
João Paulo Martins <jpamdev@gmail.com>
2022-04-01 11:35:14 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {nanoid} from 'nanoid'
+
+import AddUser from './components/Users/AddUser'
+import UsersList from './components/Users/UsersList';
 
 function App() {
+
+  const [usersList, setUsersList] = React.useState([])
+
+  const submitHandler = (userName, userAge) => {
+    setUsersList(previousUser => {
+      return [{user: userName, age: userAge, key: nanoid()}, ...previousUser]
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AddUser submitHandler={submitHandler} />
+      {usersList.length > 0 && <UsersList users={usersList}/>}
     </div>
   );
 }
